6b93a375ac2756867a29250205dea3e1c382fd2e
Jari Tulilahti <jari@rakettitiede.com>
2015-01-24 21:35:04 +0200

    Tab fix

--- uart.h
+++ uart.h
@@ -1,10 +1,10 @@
-#define state 			GPIOR0
+#define state 		GPIOR0
 #define current_byte 	GPIOR1
 
-#define UART_PORT   PORTA
-#define UART_DDR    DDRA
-#define UART_TX     PA7
+#define UART_PORT 	PORTA
+#define UART_DDR 	DDRA
+#define UART_TX 	PA7
 
-#define NONE 0
-#define START 1
-#define STOP 10
+#define NONE		0
+#define START		1
+#define STOP		10
